280e2b4dccfba73e372f721d04b934df8abb108c
eppsta <lucas.m.epperson@gmail.com>
2021-08-17 00:17:36 -0600

    updates

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,3 +6,18 @@ function scrollProjects() {
     var elem = document.getElementById("projectsSection");
     elem.scrollIntoView();
 }
+
+function open_navMenu() {
+    var x = document.getElementById("navMenu");
+        if  (x.style.display == "flex") {
+            x.style.display = "none";
+        }   else {
+            x.style.display = "flex";
+        }
+
+}
+
+
+
+
+
